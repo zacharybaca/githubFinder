6032f00cc6d5dcf1e32e6fc62ed6e5d6fd9f86dc
zacharybaca <baca.zach@icloud.com>
2020-02-18 23:09:19 -0600

    added props and propTypes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import NavBar from './components/layout/NavBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello From React</h1>
+        <NavBar />
       </div>
     );
   }
